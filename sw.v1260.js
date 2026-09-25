9fad4b4fa0fd6b2dc284128fd5dde115f13139f4
BetLab23 <nicouleau.valentin@orange.fr>
2026-09-25 18:21:04 +0200

    Add files via upload

---
+++ sw.v1260.js
@@ -0,0 +1,5 @@
+const CACHE='forgelab-shell-v1260';
+const ASSETS=['./','./index.html','./mobile.v1260.css?build=1259','./mobile.v1260.js?build=1259','./manifest.v1225.webmanifest?build=1259','./auth-laurel-realistic-v1225.png?build=1256','./icon-192-v1257.png','./icon-512-v1257.png','./icon-1024-v1257.png','./apple-touch-icon-v1257.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
