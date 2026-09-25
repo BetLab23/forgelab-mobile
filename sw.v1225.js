d674846d19a21560543aceb9665878ec0e1934e5
BetLab23 <nicouleau.valentin@orange.fr>
2026-09-25 12:12:15 +0200

    Add files via upload

--- sw.v1225.js
+++ sw.v1225.js
@@ -1,5 +1,5 @@
-const CACHE='forgelab-shell-v1225';
-const ASSETS=['./','./index.html','./mobile.v1225.css?build=1224','./mobile.v1225.js?build=1224','./manifest.v1225.webmanifest?build=1224','./auth-laurel-realistic-v1225.png?build=1224'];
+const CACHE='forgelab-shell-v1237';
+const ASSETS=['./','./index.html','./mobile.v1225.css?build=1237','./mobile.v1225.js?build=1224','./manifest.v1225.webmanifest?build=1224','./auth-laurel-realistic-v1225.png?build=1224'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil((async()=>{for(const k of await caches.keys())if(k!==CACHE)await caches.delete(k);await self.clients.claim();})());});
 self.addEventListener('fetch',e=>{
